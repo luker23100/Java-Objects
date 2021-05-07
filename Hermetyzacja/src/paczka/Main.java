c6a2d8761fa3ffec4d77150d9aa07cb723681a1e
Mikołaj <mikolajjanusz23@gmail.com>
2021-05-07 23:09:39 +0200

    #21 Completed tasks on encapsulation

---
+++ Hermetyzacja/src/paczka/Main.java
@@ -0,0 +1,25 @@
+package paczka;
+
+import java.awt.Point;
+
+import wydzialKomunikacji.Aplikacja;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//Zadanie 1
+		
+		Przechowywarka przechowaj = new Przechowywarka();
+		przechowaj.setNapis("Ceg³a");
+		przechowaj.setPunkt(new Point(2, 7));
+		//System.out.println(przechowaj.getNapis());
+		
+		//Zadanie 2
+		
+		new Aplikacja();
+		
+		
+	}
+
+}
